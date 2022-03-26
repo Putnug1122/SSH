de6d367a1ebfd29b16c8f9052b2cfa73d5dfa623
Putnug1122 <asiapatta10@gmail.com>
2022-03-26 15:20:58 +0800

    30 Days of Javascript

---
+++ day 1/variable.js
@@ -0,0 +1,5 @@
+planets = ["earth", "venus"];
+list = planets;
+planets = ["pluto", "mars"];
+console.log(list);
+console.log(planets);
---
+++ day 2/app.js
@@ -0,0 +1,11 @@
+let word = "Javascript";
+
+const pi = Math.PI;
+console.log(pi);
+myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(Math.max(...myArray));
+
+console.log(word.length);
+
+console.log(word.substring(0, 3));
+console.log(word.includes("script"));
